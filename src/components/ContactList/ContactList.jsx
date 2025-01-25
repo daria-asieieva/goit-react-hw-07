cd5c8171d78954b241c6d517b81acac494ec6d23
Daria <dariaasieieva@gmail.com>
2025-01-25 12:30:29 +0100

    2

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,30 @@
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { deleteContact } from "../../redux/contactsOps";
+import { deleteContact } from "../../redux/contactsSlice"; 
+import { selectFilteredContacts } from "../../redux/contactsSlice"; 
 import styles from "./ContactList.module.css";
 
 const ContactList = () => {
-  const contacts = useSelector((state) => state.contacts.items);
-  const filter = useSelector((state) => state.filters.name);
   const dispatch = useDispatch();
+  const filteredContacts = useSelector(selectFilteredContacts); 
 
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const handleDelete = (id) => {
+    dispatch(deleteContact(id));
+  };
 
   return (
     <ul className={styles.list}>
-      {filteredContacts.map(({ id, name, phone }) => (
+      {filteredContacts.map(({ id, name, number }) => (
         <li key={id} className={styles.item}>
-          {name}: {phone}
-          <button onClick={() => dispatch(deleteContact(id))}>Delete</button>
+          <p>
+            {name}: {number}
+          </p>
+          <button
+            onClick={() => handleDelete(id)}
+            className={styles.deleteButton}
+          >
+            Delete
+          </button>
         </li>
       ))}
     </ul>
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,5 +1,47 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { fetchContacts, addContact, deleteContact } from "./contactsOps";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { createSelector } from "reselect";
+
+
+const BASE_URL = "https://679432165eae7e5c4d90ef6c.mockapi.io/contacts"; 
+
+
+export const fetchContacts = createAsyncThunk(
+  "contacts/fetchContacts",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(BASE_URL);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  "contacts/addContact",
+  async (newContact, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(BASE_URL, newContact);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (contactId, { rejectWithValue }) => {
+    try {
+      await axios.delete(`${BASE_URL}/${contactId}`);
+      return contactId;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
 
 const initialState = {
   items: [],
@@ -7,12 +49,12 @@ const initialState = {
   error: null,
 };
 
+
 const contactsSlice = createSlice({
   name: "contacts",
   initialState,
   extraReducers: (builder) => {
     builder
-      
       .addCase(fetchContacts.pending, (state) => {
         state.loading = true;
         state.error = null;
@@ -25,9 +67,9 @@ const contactsSlice = createSlice({
         state.loading = false;
         state.error = action.payload;
       })
-      
       .addCase(addContact.pending, (state) => {
         state.loading = true;
+        state.error = null;
       })
       .addCase(addContact.fulfilled, (state, action) => {
         state.loading = false;
@@ -37,9 +79,9 @@ const contactsSlice = createSlice({
         state.loading = false;
         state.error = action.payload;
       })
-      
       .addCase(deleteContact.pending, (state) => {
         state.loading = true;
+        state.error = null;
       })
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.loading = false;
@@ -53,5 +95,20 @@ const contactsSlice = createSlice({
       });
   },
 });
+
+
+export const selectContacts = (state) => state.contacts.items;
+export const selectFilter = (state) => state.filters.name;
+
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
+
 
 export default contactsSlice.reducer;
